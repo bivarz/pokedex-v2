689ec933fba2837d2fcf663145b8b0fc4a440c0a
Bivar <paulo.bivar@gmail.com>
2025-05-25 12:32:08 -0300

    feat: adding initial config

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {" "}
+        <Card>
+          <CardContent>
+            Pokemon Card
+            <Button variant="default">here</Button>
+          </CardContent>
+        </Card>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+    </div>
+  );
+}
