c15296637d63d3acea612f18d9dd38bd823564b2
Bivar <paulo.bivar@gmail.com>
2025-05-25 15:25:29 -0300

    feat: Add Axios client configured for PokeApi

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,15 +1,22 @@
-import nextJest from "next/jest";
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   dir: "./",
 });
 
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.stories.{js,jsx,ts,tsx}",
+  ],
 };
 
-export default createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);
